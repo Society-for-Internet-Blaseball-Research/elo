cdfe149812d7ba18f033cfed51a9d23a0d4709f3
iliana etaoin <iliana@buttslol.net>
2020-08-28 00:33:15 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+extremely work in progress :) come hang out in #elo on the SIBR discord
---
+++ elo.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+import collections
+import json
+import math
+import os
+
+from pprint import pprint
+
+model = {
+    'initial': 1500,
+    'K': 5,
+}
+
+days = {}
+for root, dirs, files in os.walk('game-data'):
+    for filename in files:
+        with open(os.path.join(root, filename)) as f:
+            data= json.load(f)
+        days[(data[0]['season'], data[0]['day'])] = data
+
+ratings = collections.defaultdict(lambda: 1500)
+day_ratings = collections.defaultdict(dict)
+correct = 0
+total = 0
+for (season, day), data in sorted(days.items()):
+    for game in data:
+        away = game['awayTeamNickname']
+        home = game['homeTeamNickname']
+        r_a = ratings[away]
+        r_h = ratings[home]
+        q_a = math.pow(10, r_a / 400)
+        q_h = math.pow(10, r_h / 400)
+        e_a = q_a / (q_a + q_h)
+        e_h = q_h / (q_a + q_h)
+        s_a = int(game['awayScore'] > game['homeScore'])
+        s_h = int(game['awayScore'] < game['homeScore'])
+        r_a_new = r_a + model['K'] * (s_a - e_a)
+        r_h_new = r_h + model['K'] * (s_h - e_h)
+
+        total += 1
+        if abs(e_a - s_a) <= 0.5:
+            correct += 1
+
+        day_ratings[(season, day)][away] = r_a_new
+        day_ratings[(season, day)][home] = r_h_new
+        ratings[away] = r_a_new
+        ratings[home] = r_h_new
+
+pprint(sorted(ratings.items(), key=lambda x: x[1], reverse=True))
+print(f'accuracy: {correct / total}')
