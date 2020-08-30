30b0131dfd846fc529a1b695fd583da08e420393
iliana etaoin <iliana@buttslol.net>
2020-08-29 17:53:52 -0700

    Calculate root mean squared error

--- elo.py
+++ elo.py
@@ -11,6 +11,9 @@ model = {
     'K': 5,
 }
 
+def error(e_a, e_h, s_a, s_h):
+    return math.pow(e_a - s_a, 2) + math.pow(e_h - s_h, 2)
+
 days = {}
 for root, dirs, files in os.walk('game-data'):
     for filename in files:
@@ -18,10 +21,11 @@ for root, dirs, files in os.walk('game-data'):
             data= json.load(f)
         days[(data[0]['season'], data[0]['day'])] = data
 
-ratings = collections.defaultdict(lambda: 1500)
+ratings = collections.defaultdict(lambda: model['initial'])
+pitcher_ratings = collections.defaultdict(lambda: model['initial'])
 day_ratings = collections.defaultdict(dict)
-our_model = [0, 0]
-official_model = [0, 0]
+our_error = 0
+official_error = 0
 for (season, day), data in sorted(days.items()):
     for game in data:
         away = game['awayTeamNickname']
@@ -37,15 +41,10 @@ for (season, day), data in sorted(days.items()):
         r_a_new = r_a + model['K'] * (s_a - e_a)
         r_h_new = r_h + model['K'] * (s_h - e_h)
 
-        our_model[1] += 1
-        if abs(e_a - s_a) <= 0.5:
-            our_model[0] += 1
-
+        our_error += error(e_a, e_h, s_a, s_h)
         # ignore official odds that weren't calculated due to the data loss
         if not (season == 3 and (day == 87 or day == 88)):
-            official_model[1] += 1
-            if abs(game['awayOdds'] - s_a) <= 0.5:
-                official_model[0] += 1
+            official_error += error(game['awayOdds'], game['homeOdds'], s_a, s_h)
 
         day_ratings[(season, day)][away] = r_a_new
         day_ratings[(season, day)][home] = r_h_new
@@ -53,5 +52,5 @@ for (season, day), data in sorted(days.items()):
         ratings[home] = r_h_new
 
 pprint(sorted(ratings.items(), key=lambda x: x[1], reverse=True))
-print(f'our model accuracy:      {our_model[0] / our_model[1]}')
-print(f'official model accuracy: {official_model[0] / official_model[1]}')
+print(f'our error:      {math.sqrt(our_error)}')
+print(f'official error: {math.sqrt(official_error)}')
